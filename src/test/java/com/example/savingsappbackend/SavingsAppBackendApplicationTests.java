47b38b8b6df7da3a07764ebf50a03a3470092846
unknown <artanbayram03@gmail.com>
2024-04-17 20:42:42 +0200

    init

---
+++ src/test/java/com/example/savingsappbackend/SavingsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.savingsappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
